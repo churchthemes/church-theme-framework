b6f5086531920540525e0fa409d2cf61ed8cb10d
Steven Gliebe <work@stevengliebe.com>
2013-03-26 16:33:52 -0500

    Rename comment-list.php to comment.php

--- includes/comments.php
+++ includes/comments.php
@@ -1,22 +1,20 @@
 <?php
  
 /**
- * Comment List
+ * Comment Callback
  * 
- * comments.php can use this as callback to load a template.
+ * comments.php can use this as wp_list_comments callback to load comment.php for rendering each comment.
  */
 
-function ctc_comment_list( $comment, $args, $depth ) {
-
-	global $post;
+function ctc_comment( $comment, $args, $depth ) {
 
 	$GLOBALS['comment'] = $comment;
 	
 	// Get template to use from args
-	$template = isset( $args['ctc_template'] ) ? $args['ctc_template'] : 'comment-list.php'; // default template
-	$template = apply_filters( 'ctc_comment_list_template', $template, $comment, $args, $depth );
+	$template = isset( $args['ctc_template'] ) ? $args['ctc_template'] : 'comment.php'; // default template when ctc_template argument not passed
+	$template = apply_filters( 'ctc_comment_template', $template, $comment, $args, $depth );
 
-	// Load comment list template
+	// Load comment template for each
 	if ( $template_path = locate_template( $template ) ) {
 		include $template_path; // do manual include so variables get passed (versus using load with locate_template)
 	}
