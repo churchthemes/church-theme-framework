a7f444d773670197745a9e6c4aafa436dceb94bf
Steven Gliebe <work@stevengliebe.com>
2013-03-29 15:35:26 -0500

    Add condition checking functions

---
+++ includes/conditions.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Detect Conditions
+ */
+
+/*******************************************
+ * CONTENT
+ *******************************************/
+
+/**
+ * Is multipage
+ */
+
+function ctc_is_multipage() {
+
+	global $multipage;
+
+	$is_multipage = false;
+
+	if ( ! empty( $multipage ) ) {
+		$is_multipage = true;
+	}
+
+	return apply_filters( 'ctc_is_multipage', $is_multipage );
+
+}
+
+/*******************************************
+ * USERS
+ *******************************************/
+
+/**
+ * User can edit post
+ */
+
+function ctc_can_edit_post() {
+
+	global $post;
+
+	$can_edit = false;
+
+	if ( ! empty( $post ) ) {
+
+		$post_type_object = get_post_type_object( $post->post_type );
+
+		if ( ! empty( $post_type_object ) ) {
+
+			if ( current_user_can( $post_type_object->cap->edit_post, $post->ID ) ) {
+				$can_edit = true;
+			}
+
+		}
+
+	}
+
+	return apply_filters( 'ctc_can_edit_post', $can_edit );
+
+}
