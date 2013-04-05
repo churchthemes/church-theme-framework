8d2fd510e65e04b9d6e0fd024a28fa5975ad424b
Steven Gliebe <work@stevengliebe.com>
2013-04-05 14:01:08 -0500

    Improve ctc_sermon_data()

--- includes/sermons.php
+++ includes/sermons.php
@@ -11,7 +11,7 @@
  * Enable date archives for sermon posts
  * At time of making, WordPress (3.4 and possibly later) does not support dated archives for custom post types as it does for standard posts
  * This injects rules so that URL's like /cpt/2012/05 can be used with the custom post type archive template
- * Refer to includes/posts.php:ctc_cpt_date_archive_setup() for full details
+ * Refer to ctc_cpt_date_archive_setup() for full details
  */
 
 add_action( 'generate_rewrite_rules', 'ctc_sermon_date_archive' ); // enable date archive for sermon post type
@@ -36,29 +36,18 @@ function ctc_sermon_date_archive( $wp_rewrite ) {
  * Get sermon meta data
  */
 
-function ctc_sermon_meta() {
+function ctc_sermon_data( $post_id = null ) {
 
-	$meta = array();
+	// Get meta values
+	$meta = ctc_get_meta_data( array( // without _ccm_sermon_ prefix
+		'video_url',
+		'audio_url',
+		'pdf_url',
+		'text'
+	), $post_id );
 
-	$post_id = get_the_ID();
-
-	if ( $post_id ) {
-
-		$fields = array( // without _ccm_sermon_ prefix
-			'video_url',
-			'audio_url',
-			'pdf_url',
-			'text'
-		);
-
-		foreach( $fields as $field ) {
-			$meta[$field] = get_post_meta( $post_id, '_ccm_sermon_' . $field, true );
-		}
-
-	}
-
+	// Return filtered
 	return apply_filters( 'ctc_sermon_meta', $meta );
 
 }
-
 
