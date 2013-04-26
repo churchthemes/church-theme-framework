94e6f12bf880194056f02291f8c6f5c7b116b478
Steven Gliebe <work@stevengliebe.com>
2013-04-25 19:17:05 -0500

    Add Galleries widget for listing gallery pages

---
+++ includes/classes/widget-galleries.php
@@ -0,0 +1,189 @@
+<?php
+/**
+ * Galleries Widget
+ *
+ * This lists all gallery pages.
+ */
+
+class CTC_Widget_Galleries extends CTC_Widget {
+
+	/**
+	 * Register widget with WordPress
+	 */
+
+	function __construct() {
+
+		parent::__construct(
+			'ctc-galleries',
+			_x( 'CT Galleries', 'widget', 'church-theme' ),
+			array(
+				'description' => __( 'Shows list of gallery pages', 'church-theme' )
+			)			
+		);
+
+	}
+
+	/**
+	 * Field configuration
+	 *
+	 * This is used by CTC_Widget class for automatic field output, filtering, sanitization and saving.
+	 */
+	 
+	function ctc_fields() { // prefix in case WP core adds method with same name
+
+		// Fields
+		$fields = array(
+
+			// Example
+			/*
+			'field_id' => array(
+				'name'				=> __( 'Field Name', 'ccm' ),
+				'after_name'		=> '', // (Optional), (Required), etc.
+				'desc'				=> __( 'This is the description below the field.', 'ccm' ),
+				'type'				=> 'text', // text, textarea, checkbox, radio, select, number, url, image
+				'checkbox_label'	=> '', //show text after checkbox
+				'radio_inline'		=> false, // show radio inputs inline or on top of each other
+				'number_min'		=> '', // lowest possible value for number type
+				'number_max'		=> '', // highest possible value for number type
+				'options'			=> array(), // array of keys/values for radio or select
+				'default'			=> '', // value to pre-populate option with (before first save or on reset)
+				'no_empty'			=> false, // if user empties value, force default to be saved instead
+				'allow_html'		=> false, // allow HTML to be used in the value (text, textarea)
+				'attributes'		=> array(), // attributes to add to input element
+				'class'				=> '', // class(es) to add to input
+				'field_attributes'	=> array(), // attr => value array for field container
+				'field_class'		=> '', // class(es) to add to field container
+				'custom_sanitize'	=> '', // function to do additional sanitization (or array( &$this, 'method' ))
+				'custom_field'		=> '', // function for custom display of field input (or array( &$this, 'method' ))
+				'taxonomies'		=> array(), // hide field if taxonomies are not supported
+			);
+			*/
+
+			// Title
+			'title' => array(
+				'name'				=> _x( 'Title', 'galleries widget', 'church-theme' ),
+				'after_name'		=> '', // (Optional), (Required), etc.
+				'desc'				=> '',
+				'type'				=> 'text', // text, textarea, checkbox, radio, select, number, url, image
+				'checkbox_label'	=> '', //show text after checkbox
+				'radio_inline'		=> false, // show radio inputs inline or on top of each other
+				'number_min'		=> '', // lowest possible value for number type
+				'number_max'		=> '', // highest possible value for number type
+				'options'			=> array(), // array of keys/values for radio or select
+				'default'			=> '', // value to pre-populate option with (before first save or on reset)
+				'no_empty'			=> false, // if user empties value, force default to be saved instead
+				'allow_html'		=> false, // allow HTML to be used in the value (text, textarea)
+				'attributes'		=> array(), // attributes to add to input element
+				'class'				=> '', // class(es) to add to input
+				'field_attributes'	=> array(), // attr => value array for field container
+				'field_class'		=> '', // class(es) to add to field container
+				'custom_sanitize'	=> '', // function to do additional sanitization (or array( &$this, 'method' ))
+				'custom_field'		=> '', // function for custom display of field input (or array( &$this, 'method' ))
+				'taxonomies'		=> array(), // hide field if taxonomies are not supported
+			),
+
+			// Order By
+			'orderby' => array(
+				'name'				=> _x( 'Order By', 'galleries widget', 'church-theme' ),
+				'after_name'		=> '', // (Optional), (Required), etc.
+				'desc'				=> '',
+				'type'				=> 'select', // text, textarea, checkbox, radio, select, number, url, image
+				'checkbox_label'	=> '', //show text after checkbox
+				'radio_inline'		=> false, // show radio inputs inline or on top of each other
+				'number_min'		=> '', // lowest possible value for number type
+				'number_max'		=> '', // highest possible value for number type
+				'options'			=> array( // array of keys/values for radio or select
+					'post_title'		=> _x( 'Title', 'galleries widget order by', 'church-theme' ),
+					'post_date'			=> _x( 'Date', 'galleries widget order by', 'church-theme' ),
+				),
+				'default'			=> 'title', // value to pre-populate option with (before first save or on reset)
+				'no_empty'			=> true, // if user empties value, force default to be saved instead
+				'allow_html'		=> false, // allow HTML to be used in the value (text, textarea)
+				'attributes'		=> array(), // attributes to add to input element
+				'class'				=> '', // class(es) to add to input
+				'field_attributes'	=> array(), // attr => value array for field container
+				'field_class'		=> 'ctc-widget-no-bottom-margin', // class(es) to add to field container
+				'custom_sanitize'	=> '', // function to do additional sanitization (or array( &$this, 'method' ))
+				'custom_field'		=> '', // function for custom display of field input (or array( &$this, 'method' ))
+				'taxonomies'		=> array(), // hide field if taxonomies are not supported
+			),
+			
+			// Order
+			'order' => array(
+				'name'				=> '',
+				'after_name'		=> '', // (Optional), (Required), etc.
+				'desc'				=> '',
+				'type'				=> 'radio', // text, textarea, checkbox, radio, select, number, url, image
+				'checkbox_label'	=> '', // show text after checkbox
+				'radio_inline'		=> true, // show radio inputs inline or on top of each other
+				'number_min'		=> '', // lowest possible value for number type
+				'number_max'		=> '', // highest possible value for number type
+				'options'			=> array( // array of keys/values for radio or select
+					'asc'	=> _x( 'Low to High', 'galleries widget order', 'church-theme' ),
+					'desc'	=> _x( 'High to Low', 'galleries widget order', 'church-theme' ),
+				),
+				'default'			=> 'asc', // value to pre-populate option with (before first save or on reset)
+				'no_empty'			=> true, // if user empties value, force default to be saved instead
+				'allow_html'		=> false, // allow HTML to be used in the value (text, textarea)
+				'attributes'		=> array(), // attributes to add to input element
+				'class'				=> '', // class(es) to add to input
+				'field_attributes'	=> array(), // attr => value array for field container
+				'field_class'		=> '', // class(es) to add to field container
+				'custom_sanitize'	=> '', // function to do additional sanitization (or array( &$this, 'method' ))
+				'custom_field'		=> '', // function for custom display of field input (or array( &$this, 'method' ))
+				'taxonomies'		=> array(), // hide field if taxonomies are not supported
+			),
+
+			// Limit
+			'limit' => array(
+				'name'				=> _x( 'Limit', 'galleries widget', 'church-theme' ),
+				'after_name'		=> '', // (Optional), (Required), etc.
+				'desc'				=> _x( 'Set to 0 for unlimited.', 'galleries widget', 'church-theme' ),
+				'type'				=> 'number', // text, textarea, checkbox, radio, select, number, url, image
+				'checkbox_label'	=> '', //show text after checkbox
+				'radio_inline'		=> false, // show radio inputs inline or on top of each other
+				'number_min'		=> '0', // lowest possible value for number type
+				'number_max'		=> '', // highest possible value for number type
+				'options'			=> array(), // array of keys/values for radio or select
+				'default'			=> '0', // value to pre-populate option with (before first save or on reset)
+				'no_empty'			=> false, // if user empties value, force default to be saved instead
+				'allow_html'		=> false, // allow HTML to be used in the value (text, textarea)
+				'attributes'		=> array(), // attributes to add to input element
+				'class'				=> '', // class(es) to add to input
+				'field_attributes'	=> array(), // attr => value array for field container
+				'field_class'		=> '', // class(es) to add to field container
+				'custom_sanitize'	=> '', // function to do additional sanitization (or array( &$this, 'method' ))
+				'custom_field'		=> '', // function for custom display of field input (or array( &$this, 'method' ))
+				'taxonomies'		=> array(), // hide field if taxonomies are not supported
+			),
+			
+			// Hierarchy
+			'show_hierarchy' => array(
+				'name'				=> '',
+				'after_name'		=> '', // (Optional), (Required), etc.
+				'desc'				=> '',
+				'type'				=> 'checkbox', // text, textarea, checkbox, radio, select, number, url, image
+				'radio_inline'		=> false, // show radio inputs inline or on top of each other
+				'number_min'		=> '', // lowest possible value for number type
+				'number_max'		=> '', // highest possible value for number type
+				'checkbox_label'	=> _x( 'Show hierarchy', 'galleries widget', 'church-theme' ), //show text after checkbox
+				'options'			=> array(), // array of keys/values for radio or select
+				'default'			=> true, // value to pre-populate option with (before first save or on reset)
+				'no_empty'			=> false, // if user empties value, force default to be saved instead
+				'allow_html'		=> false, // allow HTML to be used in the value (text, textarea)
+				'attributes'		=> array(), // attributes to add to input element
+				'class'				=> '', // class(es) to add to input
+				'field_attributes'	=> array(), // attr => value array for field container
+				'field_class'		=> 'ctc-widget-no-bottom-margin', // class(es) to add to field container
+				'custom_sanitize'	=> '', // function to do additional sanitization (or array( &$this, 'method' ))
+				'custom_field'		=> '', // function for custom display of field input (or array( &$this, 'method' ))
+				'taxonomies'		=> array(), // hide field if taxonomies are not supported
+			),
+
+		);
+		
+		return $fields;
+	
+	}
+
+}
