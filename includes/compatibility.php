2a3a61ffca6573edc2c23c63b6ad8eea4610b03b
Steven Gliebe <work@stevengliebe.com>
2013-05-07 15:25:45 -0500

    Add theme support for minimum version of WordPress

--- includes/compatibility.php
+++ includes/compatibility.php
@@ -2,7 +2,117 @@
 /**
  * Compatibility Functions
  */
- 
+
+/*******************************************
+ * WORDPRESS
+ *******************************************/
+
+/* Based on code from default Twenty Thirteen theme */
+
+
+
+// ADD THEME SUPPORT / VERSION TO FUNCS BELOW!
+// ADD THEME SUPPORT / VERSION TO FUNCS BELOW!
+// ADD THEME SUPPORT / VERSION TO FUNCS BELOW!
+// ADD THEME SUPPORT / VERSION TO FUNCS BELOW!
+// ADD THEME SUPPORT / VERSION TO FUNCS BELOW!
+
+/**
+ * Detect if old WordPress version used
+ */
+
+function ctc_old_wp() {
+
+	$old = false;
+
+	// Theme uses this feature
+	if ( $support = get_theme_support( 'ctc-wordpress-version' ) ) { // returns false if feature not supported
+
+		// Get minimum required version
+		$required_version = isset( $support[0] ) ? $support[0] : '';
+
+		// Is old version used?
+		if ( version_compare( $GLOBALS['wp_version'], $required_version, '<' ) ) {
+			$old = true;
+		}
+
+	}
+
+	return apply_filters( 'ctc_old_wp', $old );
+
+}
+
+/**
+ * Message to show when old version used
+ */
+
+function ctc_old_wp_message() {
+
+	return __( 'The theme you selected requires a newer version of WordPress. Please update and try again.', 'church-theme' );
+
+}
+
+/**
+ * Prevent switching to theme on old version of WordPress
+ * 
+ * Switches to the previously activated theme or the default theme.
+ */
+
+add_action( 'after_switch_theme', 'ctc_old_wp_switch_theme', 10, 2 );
+
+function ctc_old_wp_switch_theme( $theme_name, $theme ) {
+
+	// Is WordPress version too old for theme?
+	if ( ctc_old_wp() ) {
+
+		if ( CTC_TEMPLATE != $theme->get_template() ) {
+			switch_theme( $theme->get_template(), $theme->get_stylesheet() );
+		} elseif ( CTC_TEMPLATE != WP_DEFAULT_THEME ) {
+			switch_theme( WP_DEFAULT_THEME );
+		}
+
+		unset( $_GET['activated'] );
+
+		add_action( 'admin_notices', 'ctc_old_wp_switch_theme_notice' );
+
+	}
+
+}
+
+/**
+ * Show notice if try to switch to theme while using old version of WordPress
+ */
+
+function ctc_old_wp_switch_theme_notice() {
+
+	?>
+	<div class="error">
+		<p>
+			<?php echo ctc_old_wp_message(); ?>
+		</p>
+	</div>
+	<?php
+
+}
+
+/**
+ * Prevent Customizer preview from showing theme while using old version of WordPress
+ */
+
+add_action( 'load-customize.php', 'ctc_old_wp_customizer_notice' );
+
+function ctc_old_wp_customizer_notice() {
+
+	// Is WordPress version too old for theme?
+	if ( ctc_old_wp() ) {
+
+		// Show message
+		wp_die( ctc_old_wp_message() . sprintf( ' <a href="javascript:history.go(-1);">%s</a>', __( 'Go back.', 'church-theme' ) ) );
+
+	}
+
+}
+
 /*******************************************
  * BROWSERS
  *******************************************/
