7d010adea09230829142996fb98259eb5c2f4061
Steven Gliebe <work@stevengliebe.com>
2013-05-08 12:13:57 -0500

    Add add_theme_support( 'ctc-taxonomy-order-note', $url ) for plugin recommendation

--- includes/taxonomies.php
+++ includes/taxonomies.php
@@ -53,3 +53,59 @@ function ctc_term_options( $taxonomy_name, $prepend = array() ) {
 	return apply_filters( 'ctc_term_options', $options, $taxonomy_name, $prepend );
 
 }
+
+/**
+ * Show custom ordering tip
+ *
+ * add_theme_support( 'ctc-taxonomy-order-note', $url ) will cause a taxonomy ordering plugin
+ * to be recommended in a note beneath taxonomy lists. $url is to override the default recommendation.
+ *
+ * This is handy in particular for people groups and sermon speakers.
+ */
+
+add_action( 'admin_init', 'ctc_taxonomy_order_notes' );
+
+function ctc_taxonomy_order_notes() {
+
+	// Only if theme supports it
+	if ( ! current_theme_supports( 'ctc-taxonomy-order-note' ) ) {
+		return false;
+	}
+
+	// Get public taxonomies
+	$taxonomies = get_taxonomies( array(
+		'public'	=> true,
+		'show_ui'	=> true // weed out post_format
+	) );
+
+	// Add note to each
+	foreach ( $taxonomies as $taxonomy ) {
+		add_action( 'after-' . $taxonomy . '-table', 'ctc_taxonomy_order_note' );
+	}
+
+}
+
+function ctc_taxonomy_order_note( $taxonomy ) {
+
+	// Only if theme requests this
+	if ( $support = get_theme_support( 'ctc-taxonomy-order-note' ) ) { // returns false if feature not supported
+
+		// Get URL if not using default
+		$url = isset( $support[0] ) ? $support[0] : 'http://churchthemes.com/go/taxonomy-order';
+
+		// Get taxonomy plural
+		$taxonomy_obj = get_taxonomy( $taxonomy );
+		$taxonomy_plural = strtolower( $taxonomy_obj->labels->name );
+
+		// Show message
+		echo '<p class="description">';
+		printf(
+			__( '<b>Custom Ordering:</b> Try <a href="%s" target="_blank">this plugin</a> for custom ordering your %s.', 'church-theme' ),
+			$url,
+			$taxonomy_plural
+		);
+		echo '</p>';
+
+	}
+
+}
