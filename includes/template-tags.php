298171365eff77ee083502607dd74d7f4dcd349d
Steven Gliebe <work@stevengliebe.com>
2013-05-10 10:30:34 -0500

    Add ctc_post_date() for showing date, 'Today' or 'Yesterday'

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -5,36 +5,58 @@
  * These output common elements for different post types.
  */
 
+
 /**
- * X Days Ago or Date
+ * Post Date
  *
- * This takes a timestamp and shows "X days ago"
- * If it is older than a certain number of days, the full date it shown
+ * Output or return the localized, formatted post date, "Today" or "Yesterday", according to options.
  */
 
-function ctc_date_ago( $timestamp, $max_days = false ) {
+function ctc_post_date( $options = array() ) {
 
-	// the current time
-	$current_time = current_time( 'timestamp' );
-	
-	// at what time in past would $max_days be exceeded?
-	$max_seconds = $max_days * 86400; // days multiplied by number of seconds in a day
-	$cutoff_time = $current_time - $max_seconds; // now minus X days ago
-	
-	// if cutoff time is newer than X days ago, show human time difference (X days/hours/minutes/seconds ago)
-	if ( $timestamp > $cutoff_time ) {
-		$time_diff = human_time_diff( $timestamp, $current_time ); // http://codex.wordpress.org/Function_Reference/human_time_diff
-		/* translators: helps form 'X days ago' with 'X days' being localized by core WordPress translation */
-		$date = sprintf( __( '%s ago' , 'church-theme' ), $time_diff ); // localized text for "X days ago" where "X days" is from WP core - to change days, must set core translation
+	$date_formatted = '';
+
+	// Default options
+	$defaults = apply_filters( 'ctc_post_date_default_options', array(
+		'return'	=> false,
+		'today'		=> true,	// show "Today" if post is from today
+		'yesterday'	=> true,	// show "Yesterday" instead of yesterday's date
+	) );
+	$options = wp_parse_args( $options, $defaults );
+
+	// Today and yesterday in local time
+	$today_ymd = date_i18n( 'Y-m-d' );
+	$yesterday_ymd = date_i18n( 'Y-m-d', strtotime( $today_ymd ) - DAY_IN_SECONDS );
+
+	// Post date
+	$date_timestamp = get_the_time( 'U' );
+	$date_ymd = date_i18n( 'Y-m-d', $date_timestamp );
+
+	// Show "Today"
+	if ( $options['today'] && $today_ymd == $date_ymd ) {
+		$date_formatted = __( 'Today', 'church-theme' );
 	}
-	
-	// timestamp is older than X days ago, show full date format
+
+	// Show "Yesterday"
+	elseif ( $options['yesterday'] && $yesterday_ymd == $date_ymd ) {
+		$date_formatted = __( 'Yesterday', 'church-theme' );
+	}
+
+	// Show date
 	else {
 		$date_format = get_option( 'date_format' ); // this is from WordPress general settings
-		$date = date_i18n( $date_format, $timestamp ); // translated date
-	}	
+		$date_formatted = date_i18n( $date_format, $date_timestamp ); // translated date
+	}
 
-	// return formatted date
-	return apply_filters( 'ctc_date_ago', $date, $timestamp, $max_days );
+	// Date filtering
+	$date_formatted = apply_filters( 'ctc_post_date', $date_formatted, $options );
+
+	// Output or return
+	if ( $options['return'] ) {
+		return $date_formatted;
+	} else {
+		echo  $date_formatted;
+	}
 
 }
+
