c90ba9651b18f93796f137ecdc6877ac6cce361a
Steven Gliebe <work@stevengliebe.com>
2013-05-18 12:41:34 -0500

    Remove gap beneath Blip.tv embed when responsive

--- js/responsive-embeds.js
+++ js/responsive-embeds.js
@@ -4,6 +4,9 @@
 
 jQuery(document).ready(function($) {
 
+	// Remove <object> element from Blip.tv (use iframe only) - creates a gap w/FitVid
+	$( "embed[src*='blip.tv']").remove();
+
 	// Use FitVid for responsive videos and other embeds
 	// YouTube and Vimeo work out of the box
 	// Rdio and Spotify are correct when loading at final size (browser resize is bad demo)
@@ -25,5 +28,6 @@ jQuery(document).ready(function($) {
 	// Other embedded media only need max-width: 100% (height is static) - SoundCloud, MediaElement.js, etc.
 	// Important: when done via stylesheet, MediaElement.js volume control flickers
 	$( "iframe[src*='soundcloud.com'], iframe[src*='snd.sc'], .wp-video-shortcode, .wp-audio-shortcode" ).css( 'max-width', '100%' );
+
 
 });
