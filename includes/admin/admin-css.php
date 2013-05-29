32b6875fdbf4dd1c4bac807b89dcef9cf955b420
Steven Gliebe <work@stevengliebe.com>
2013-05-29 17:39:47 -0500

    Remove unused ctfw_admin_body_classes()

--- includes/admin/admin-css.php
+++ includes/admin/admin-css.php
@@ -26,32 +26,7 @@ function ctfw_admin_enqueue_styles() {
 
 	// Theme license
 	if ( 'appearance_page_theme-license' == $screen->base ) {
-		wp_enqueue_style( 'ctc-license', ctc_theme_url( CTFW_CSS_DIR . '/admin-license.css' ), false, CTFW_THEME_VERSION ); // bust cache on update
+		wp_enqueue_style( 'ctfw-license', ctc_theme_url( CTFW_CSS_DIR . '/admin-license.css' ), false, CTFW_THEME_VERSION ); // bust cache on update
 	}
-	
-}
-
-/*******************************************
- * BODY CLASSES
- *******************************************/
-
-/**
- * Add helper classes to admin <body> for easier style tweaks (such as hiding "Preview" button per post type)
- */
-
-add_filter( 'admin_body_class', 'ctfw_admin_body_classes' );
- 
-function ctfw_admin_body_classes( $classes ) {
-
-	// Add useful get_current_screen() values
-	$screen = get_current_screen();
-	$screen_keys = array( 'action', 'base', 'id', 'post_type', 'taxonomy' );
-	foreach( $screen_keys as $screen_key ) {
-		if ( ! empty( $screen->$screen_key ) ) {
-			$classes .= 'ctc-screen-' . $screen_key . '-' . $screen->$screen_key . ' '; // space at end to prevent run-together
-		}
-	}
-	
-	return $classes;
 	
 }
