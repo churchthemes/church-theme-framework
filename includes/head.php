0aa9a67396f63cf4525e6c8c0564d81a7717f4c2
Steven Gliebe <work@stevengliebe.com>
2013-05-30 15:12:39 -0500

    Make wp_title show page number

--- includes/head.php
+++ includes/head.php
@@ -19,12 +19,25 @@ add_filter( 'wp_title', 'ctfw_head_title', 10, 3 );
 
 function ctfw_head_title( $title, $sep, $seplocation ) {
 
+	// Feed
+	if ( is_feed() ) {
+		return $title;
+	}
+
+	// Page number
+	$page_number = '';
+	$page = ctfw_page_num();
+	if ( $page > 1 ) {
+		/* translators: page number in <title> */
+		$page_number = sprintf( _x( ' (Page %d)', 'head title', 'church-theme-framework' ), $page );
+	}
+
 	// Homepage (site name - tagline )
 	if ( is_front_page() ) {
-		$before = get_bloginfo( 'name' );
-		$after = get_bloginfo( 'description' );
+		$before = get_bloginfo( 'name', 'display' );
+		$after = $page <= 1 ? get_bloginfo( 'description', 'display' ) : ''; // show tagline if on first page (not showing page number)
 	}
-	
+
 	// Subpage (page title - site name)
 	else {
 		$before = $title;
@@ -32,11 +45,12 @@ function ctfw_head_title( $title, $sep, $seplocation ) {
 	}
 	
 	// Build title
-	$before = trim( $before );
+	$before = trim( $before ) . $page_number;
 	$after = trim( $after );
 	$new_title = $before;
 	if ( $after ) {
-		$new_title .= _x( ' - ', 'title delimiter', 'church-theme-framework' ) . $after;
+		/* translators: delimiter for <title> content */ 
+		$new_title .= _x( ' - ', 'head title', 'church-theme-framework' ) . $after;
 	}
 	
 	return $new_title;
