fdeffc2df95e3a3c2231c5c7bedd8a6f3875aa61
Steven Gliebe <work@stevengliebe.com>
2013-06-04 13:56:21 -0500

    Add theme support for ctfw-featured-image-notes

--- includes/images.php
+++ includes/images.php
@@ -6,8 +6,97 @@
 // No direct access
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+
 /***********************************************
- * IMAGES
+ * SIZES
+ ***********************************************/
+
+/**
+ * Output image size dimensions
+ *
+ * Pass in image size to return 123x123
+ */
+
+function ctfw_image_size_dimensions( $size ) {
+
+	global $_wp_additional_image_sizes;
+
+	$dimensions = '';
+
+	if ( isset( $_wp_additional_image_sizes[$size] ) ) {
+		$dimensions = $_wp_additional_image_sizes[$size]['width'] . 'x' . $_wp_additional_image_sizes[$size]['height'];
+	}
+
+	return apply_filters( 'ctfw_image_size_dimensions', $dimensions );
+
+
+}
+
+/**
+ * Add note below Featured Image
+ *
+ * Each post type has a recommended image size.
+ * See includes/support.php for notes on Highlight and Slide widgets.
+ */
+
+add_filter( 'admin_post_thumbnail_html', 'ctfw_featured_image_notes' );
+ 
+function ctfw_featured_image_notes( $content ) {
+
+	// Theme supports this?
+	$support = get_theme_support( 'ctfw-featured-image-notes' );
+	if ( ! empty( $support[0] ) ) {
+
+		// Get post type size data
+		$post_types = $support[0];
+
+		// Get admin screen
+		$screen = get_current_screen();
+		if ( ! empty( $screen->post_type ) && ! empty( $post_types[$screen->post_type] ) ) {
+		
+			// Get post type data
+			$post_type_data = $post_types[$screen->post_type];
+
+			// Size and specific message specified
+			if ( is_array( $post_type_data ) && ! empty( $post_type_data[0] ) && ! empty( $post_type_data[1] ) ) {
+				$size = $post_type_data[0];
+				$message = $post_type_data[1];
+			}
+
+			// Only size specified (use default message)
+			elseif ( ! empty( $post_types[$screen->post_type] ) ) {
+				$size = $post_types[$screen->post_type];
+				$message = ! empty( $support[1] ) ? $support[1] : __( 'The target image size is %s.', 'ctfw' ); // third argument, if any
+			}
+
+			// Show message
+			if ( isset( $size ) && isset( $message ) ) {
+
+				// Get dimensions for size
+				$dimensions = ctfw_image_size_dimensions( $size );
+				if ( $dimensions ) {
+
+					// Add dimensions to message
+					$message = sprintf( $message, $dimensions );
+
+					// Apply the note for the appropriate post type
+					$content .= '<p class="description">' . esc_html( $message  ) . '</p>';
+
+				}
+
+			}
+
+		}
+
+	}
+
+	// Return content with note appended
+	return $content;
+	
+}
+
+/***********************************************
+ * RESIZING
  ***********************************************/
 
 /**
@@ -49,26 +138,5 @@ function ctfw_image_resize_dimensions_upscale( $output, $orig_w, $orig_h, $dest_
 	}
 
 	return $output;
-
-}
-
-/**
- * Output image size dimensions
- *
- * Pass in image size to return 123x123
- */
-
-function ctfw_image_size_dimensions( $size ) {
-
-	global $_wp_additional_image_sizes;
-
-	$dimensions = '';
-
-	if ( isset( $_wp_additional_image_sizes[$size] ) ) {
-		$dimensions = $_wp_additional_image_sizes[$size]['width'] . 'x' . $_wp_additional_image_sizes[$size]['height'];
-	}
-
-	return apply_filters( 'ctfw_image_size_dimensions', $dimensions );
-
 
 }
