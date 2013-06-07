0da81145afd2d95f52310c7dccb20c9497f1ae05
Steven Gliebe <work@stevengliebe.com>
2013-06-07 17:25:45 -0500

    Replace support for sermon Category with Topic taxonomy

--- includes/content-types.php
+++ includes/content-types.php
@@ -35,7 +35,7 @@ function ctfw_content_types() {
 
 		'sermon' => array(
 			'post_types'		=> array( 'ccm_sermon' ),
-			'taxonomies'		=> array( 'ccm_sermon_category', 'ccm_sermon_tag', 'ccm_sermon_speaker' ),
+			'taxonomies'		=> array( 'ccm_sermon_topic', 'ccm_sermon_tag', 'ccm_sermon_speaker' ),
 			'page_templates'	=> array(), // should be populated via ctfw_content_types filter in theme
 			'conditions'		=> array(),
 		),
