e00b6adcc42b94bde06faff9f5fd76714f88f9ec
Steven Gliebe <work@stevengliebe.com>
2013-09-18 16:33:24 -0500

    Make IE Unsupported friendlier with caching plugins

--- js/ie-unsupported.js
+++ js/ie-unsupported.js
@@ -1,18 +1,19 @@
+jQuery( document ).ready( function( $ ) {
 
-// Is old version of IE used?
-if ( navigator.userAgent.match( new RegExp( "MSIE [5-" + ctfw_ie_unsupported.version + "]", "gi" ) ) ) {
+	// Is old version of IE used?
+	if ( navigator.userAgent.match( new RegExp( "MSIE [5-" + ctfw_ie_unsupported.version + "]", "gi" ) ) ) {
 
-	// Hide content
-	jQuery( document ).ready( function( $ ) {
+		// Hide content
 		$( 'body' )
 			.empty() // remove content
 			.css( 'background', 'none' ); // hide background
-	} );
 
-	// Tell user to upgrade to a modern browser
-	alert( ctfw_ie_unsupported.message );
+		// Tell user to upgrade to a modern browser
+		alert( ctfw_ie_unsupported.message );
 
-	// Redirect to a site with upgrade details
-	window.location = ctfw_ie_unsupported.redirect_url;
+		// Redirect to a site with upgrade details
+		window.location = ctfw_ie_unsupported.redirect_url;
 
-}
+	}
+
+});
