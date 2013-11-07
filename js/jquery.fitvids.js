c6e1251b140db9c6832aec71255d280191809645
Steven Gliebe <work@stevengliebe.com>
2013-11-07 16:20:38 -0600

    Update FitVids.js

--- js/jquery.fitvids.js
+++ js/jquery.fitvids.js
@@ -3,7 +3,7 @@
 /*!
 * FitVids 1.0
 *
-* Copyright 2011, Chris Coyier - http://css-tricks.com + Dave Rupert - http://daverupert.com
+* Copyright 2013, Chris Coyier - http://css-tricks.com + Dave Rupert - http://daverupert.com
 * Credit to Thierry Koblentz - http://www.alistapart.com/articles/creating-intrinsic-ratios-for-video/
 * Released under the WTFPL license - http://sam.zoy.org/wtfpl/
 *
@@ -22,28 +22,13 @@
     if(!document.getElementById('fit-vids-style')) {
 
       var div = document.createElement('div'),
-          ref = document.getElementsByTagName('base')[0] || document.getElementsByTagName('script')[0];
+          ref = document.getElementsByTagName('base')[0] || document.getElementsByTagName('script')[0],
+          cssStyles = '&shy;<style>.fluid-width-video-wrapper{width:100%;position:relative;padding:0;}.fluid-width-video-wrapper iframe,.fluid-width-video-wrapper object,.fluid-width-video-wrapper embed {position:absolute;top:0;left:0;width:100%;height:100%;}</style>';
 
       div.className = 'fit-vids-style';
       div.id = 'fit-vids-style';
       div.style.display = 'none';
-      div.innerHTML = '&shy;<style>         \
-        .fluid-width-video-wrapper {        \
-           width: 100%;                     \
-           position: relative;              \
-           padding: 0;                      \
-        }                                   \
-                                            \
-        .fluid-width-video-wrapper iframe,  \
-        .fluid-width-video-wrapper object,  \
-        .fluid-width-video-wrapper embed {  \
-           position: absolute;              \
-           top: 0;                          \
-           left: 0;                         \
-           width: 100%;                     \
-           height: 100%;                    \
-        }                                   \
-      </style>';
+      div.innerHTML = cssStyles;
 
       ref.parentNode.insertBefore(div,ref);
 
@@ -85,4 +70,5 @@
       });
     });
   };
-})( jQuery );
+// Works with either jQuery or Zepto
+})( window.jQuery || window.Zepto );
