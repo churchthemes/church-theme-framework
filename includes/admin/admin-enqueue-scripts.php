a32efd51a9d686a3812128a20f84e57518553533
Steven Gliebe <work@stevengliebe.com>
2014-04-16 16:49:06 -0500

    Add ctfw_admin_widgets_js_data() to use everywhere admin-widgets.js is enqueued

--- includes/admin/admin-enqueue-scripts.php
+++ includes/admin/admin-enqueue-scripts.php
@@ -33,12 +33,7 @@ function ctfw_admin_enqueue_scripts() {
 
 		// Main widgets script
 		wp_enqueue_script( 'ctfw-admin-widgets', ctfw_theme_url( CTFW_JS_DIR . '/admin-widgets.js' ), array( 'jquery' ), CTFW_THEME_VERSION ); // bust cache on update
-		wp_localize_script( 'ctfw-admin-widgets', 'ctfw_widgets', array( // make data available
-			'image_library_title'	=> _x( 'Choose Image for Widget', 'widget image library', 'church-theme-framework' ),
-			'image_library_button'	=> _x( 'Use in Widget', 'widget image library', 'church-theme-framework' ),
-			'incompatible_message'	=> __( 'Sorry, this widget is not made for use in this area. Please delete.', 'church-theme-framework' ),
-			'widget_restrictions'	=> current_theme_supports( 'ctfw-sidebar-widget-restrictions' )
-		));
+		wp_localize_script( 'ctfw-admin-widgets', 'ctfw_widgets', ctfw_admin_widgets_js_data() ); // see admin-widgets.php
 
 	}
 
--- includes/admin/admin-widgets.php
+++ includes/admin/admin-widgets.php
@@ -4,7 +4,7 @@
  *
  * @package    Church_Theme_Framework
  * @subpackage Admin
- * @copyright  Copyright (c) 2013, churchthemes.com
+ * @copyright  Copyright (c) 2013-2014, churchthemes.com
  * @link       https://github.com/churchthemes/church-theme-framework
  * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * @since      0.9
@@ -12,6 +12,33 @@
 
 // No direct access
 if ( ! defined( 'ABSPATH' ) ) exit;
+
+/*******************************************
+ * DATA
+ *******************************************/
+
+/**
+ * Admin widget JavaScript data
+ *
+ * This is for passing into localization for JavaScript when admin-widget.js is enqueued.
+ * It's functionized because admin-widget.js is inqueued in two places: Appearance > Widgets and Customize
+ *
+ * @since 1.1.5
+ * @return array Data for JavaScript
+ */
+
+function ctfw_admin_widgets_js_data() {
+
+	$data = array( // make data available
+		'image_library_title'	=> _x( 'Choose Image for Widget', 'widget image library', 'church-theme-framework' ),
+		'image_library_button'	=> _x( 'Use in Widget', 'widget image library', 'church-theme-framework' ),
+		'incompatible_message'	=> __( 'Sorry, this widget is not made for use in this area. Please delete.', 'church-theme-framework' ),
+		'widget_restrictions'	=> current_theme_supports( 'ctfw-sidebar-widget-restrictions' )
+	);
+
+	return apply_filters( 'ctfw_admin_widgets_js_data', $data );
+
+}
 
 /*******************************************
  * WIDGET RESTRICTIONS
