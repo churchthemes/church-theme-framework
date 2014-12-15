0f89e3a41b9b503a1df2b55374d240bd6b31bb2c
Steven Gliebe <work@stevengliebe.com>
2014-12-15 11:45:27 -0600

    Add backwards compatibility for new title-tag theme feature via ctfw_title_tag_compat()

--- includes/head.php
+++ includes/head.php
@@ -6,7 +6,7 @@
  *
  * @package    Church_Theme_Framework
  * @subpackage Functions
- * @copyright  Copyright (c) 2013, churchthemes.com
+ * @copyright  Copyright (c) 2013 - 2014, churchthemes.com
  * @link       https://github.com/churchthemes/church-theme-framework
  * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * @since      0.9
@@ -20,7 +20,28 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  *******************************************/
 
 /**
- * Automatic <title>
+ * Title tag theme support backwards compatibility
+ *
+ * Themes should use add_theme_support( 'title-tag' ) to add the <title> tag as of WordPRess 4.1.
+ *
+ * This provides backward-compatibility by adding the title tag to <head> in older versions.
+ *
+ * @since 1.4
+ */
+function ctfw_title_tag_compat() {
+
+	// Manually insert <title> tag if WordPress doesn't support title-tag
+	// But, only if the theme is trying to add title-tag support
+	if ( ! function_exists( '_wp_render_title_tag' ) && current_theme_supports( 'title-tag' ) ) {
+		echo "<title>" . wp_title( '', false, 'right' ) . "</title>\n"; // ctfw_head_title() below modifies this
+	}
+
+}
+
+add_action( 'wp_head', 'ctfw_title_tag_compat', 1 );
+
+/**
+ * Filter <title> tag to be friendly
  *
  * An SEO plugin can be used to fine-tune the <title> for various areas of the site.
  *
