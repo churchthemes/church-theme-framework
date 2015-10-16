efb0e363a915edd765e07155d6f0f5d094d9d5ba
Steven Gliebe <work@stevengliebe.com>
2015-10-16 15:46:30 -0500

    Deprecate ctfw_head_title() / 'ctfw-auto-title'; Use add_theme_support( 'title-tag' ) instead

--- includes/deprecated.php
+++ includes/deprecated.php
@@ -6,7 +6,7 @@
  *
  * @package    Church_Theme_Framework
  * @subpackage Functions
- * @copyright  Copyright (c) 2013 - 2014, churchthemes.com
+ * @copyright  Copyright (c) 2013 - 2015, churchthemes.com
  * @link       https://github.com/churchthemes/church-theme-framework
  * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * @since      0.9
@@ -19,6 +19,70 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * Possible candidate may be ctfw_theme_url() which could end up in WordPress core as
  * something like theme_url() at some point: http://core.trac.wordpress.org/ticket/18302
  */
+
+/**
+ * Filter <title> tag to be friendly
+ *
+ * An SEO plugin can be used to fine-tune the <title> for various areas of the site.
+ *
+ * Deprecation: add_theme_support( 'title-tag' ) in WordPress 4.1 handles this nicely.
+ *
+ * @since 0.9
+ * @deprecated 1.7.3
+ * @param string $title Page title determined by WordPress core
+ * @param string $sep Optional, default is '&raquo;'. How to separate the various items within the page title.
+ * @param string $seplocation Optional. Direction to display title, 'right'.
+ * @return string Formatted title
+ */
+function ctfw_head_title( $title, $sep, $seplocation ) {
+
+	_deprecated_function( __FUNCTION__, '1.7.3', "add_theme_support( 'title-tag' )" );
+
+	$new_title = $title;
+
+	if ( current_theme_supports( 'ctfw-auto-title' ) ) {
+
+		// Feed
+		if ( is_feed() ) {
+			return $title;
+		}
+
+		// Page number
+		$page_number = '';
+		$page = ctfw_page_num();
+		if ( $page > 1 ) {
+			/* translators: page number in <title> */
+			$page_number = sprintf( _x( ' (Page %d)', 'head title', 'church-theme-framework' ), $page );
+		}
+
+		// Homepage (site name - tagline )
+		if ( is_front_page() ) {
+			$before = get_bloginfo( 'name', 'display' );
+			$after = $page <= 1 ? get_bloginfo( 'description', 'display' ) : ''; // show tagline if on first page (not showing page number)
+		}
+
+		// Subpage (page title - site name)
+		else {
+			$before = $title;
+			$after = get_bloginfo( 'name' );
+		}
+
+		// Build title
+		$before = trim( $before ) . $page_number;
+		$after = trim( $after );
+		$new_title = $before;
+		if ( $after ) {
+			/* translators: separator for <title> content */
+			$new_title .= _x( ' - ', 'head title', 'church-theme-framework' ) . $after;
+		}
+
+	}
+
+	return $new_title;
+
+}
+
+add_filter( 'wp_title', 'ctfw_head_title', 10, 3 );
 
 /**
  * @since 0.9
--- includes/head.php
+++ includes/head.php
@@ -20,65 +20,6 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  *******************************************/
 
 /**
- * Filter <title> tag to be friendly
- *
- * An SEO plugin can be used to fine-tune the <title> for various areas of the site.
- *
- * @since 0.9
- * @param string $title Page title determined by WordPress core
- * @param string $sep Optional, default is '&raquo;'. How to separate the various items within the page title.
- * @param string $seplocation Optional. Direction to display title, 'right'.
- * @return string Formatted title
- */
-function ctfw_head_title( $title, $sep, $seplocation ) {
-
-	$new_title = $title;
-
-	if ( current_theme_supports( 'ctfw-auto-title' ) ) {
-
-		// Feed
-		if ( is_feed() ) {
-			return $title;
-		}
-
-		// Page number
-		$page_number = '';
-		$page = ctfw_page_num();
-		if ( $page > 1 ) {
-			/* translators: page number in <title> */
-			$page_number = sprintf( _x( ' (Page %d)', 'head title', 'church-theme-framework' ), $page );
-		}
-
-		// Homepage (site name - tagline )
-		if ( is_front_page() ) {
-			$before = get_bloginfo( 'name', 'display' );
-			$after = $page <= 1 ? get_bloginfo( 'description', 'display' ) : ''; // show tagline if on first page (not showing page number)
-		}
-
-		// Subpage (page title - site name)
-		else {
-			$before = $title;
-			$after = get_bloginfo( 'name' );
-		}
-
-		// Build title
-		$before = trim( $before ) . $page_number;
-		$after = trim( $after );
-		$new_title = $before;
-		if ( $after ) {
-			/* translators: separator for <title> content */
-			$new_title .= _x( ' - ', 'head title', 'church-theme-framework' ) . $after;
-		}
-
-	}
-
-	return $new_title;
-
-}
-
-add_filter( 'wp_title', 'ctfw_head_title', 10, 3 );
-
-/**
  * Add event calendar month and category to <title> tag
  *
  * This compliments any existing title whether default or a plugin like Yoast SEO.
