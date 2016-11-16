60ab3c9b42b18310889989404b626f3158f7c169
Steven Gliebe <work@stevengliebe.com>
2016-11-16 16:08:04 -0600

    Add menus.php and 'ctfw-nav-add-title-attr' feature

--- framework.php
+++ framework.php
@@ -152,6 +152,9 @@ $ctfw_includes = array(
 	// Frontend Only
 	'frontend' => array (
 
+		// Functions
+		CTFW_INC_DIR . '/menus.php',
+
 		// Classes
 		CTFW_CLASS_DIR . '/breadcrumbs.php',
 		CTFW_CLASS_DIR . '/walker-nav-menu-description.php',
