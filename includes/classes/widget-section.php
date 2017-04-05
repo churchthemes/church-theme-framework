2a4357d0b9d5347a986274f41647f3a315d8fd28
Steven Gliebe <work@stevengliebe.com>
2017-04-05 12:53:39 -0500

    Add Attachment (Scroll, Fixed) setting to CT Section widget

--- includes/classes/widget-section.php
+++ includes/classes/widget-section.php
@@ -234,7 +234,7 @@ class CTFW_Widget_Section extends CTFW_Widget {
 				'taxonomies'		=> array(), // hide field if taxonomies are not supported
 			),
 
-			// Image Brightness
+			// Brightness
 			'image_brightness' => array(
 				'name'				=> _x( 'Brightness', 'section widget', 'church-theme-framework' ),
 				'after_name'		=> _x( '(Percentage)', 'section widget', 'church-theme-framework' ), // (Optional), (Required), etc.
@@ -262,7 +262,7 @@ class CTFW_Widget_Section extends CTFW_Widget {
 				'taxonomies'		=> array(), // hide field if taxonomies are not supported
 			),
 
-			// Image Opacity
+			// Opacity
 			'image_opacity' => array(
 				'name'				=> _x( 'Opacity', 'section widget', 'church-theme-framework' ),
 				'after_name'		=> _x( '(Percentage)', 'section widget', 'church-theme-framework' ), // (Optional), (Required), etc.
@@ -282,6 +282,35 @@ class CTFW_Widget_Section extends CTFW_Widget {
 				'attributes'		=> array(
 										'style="width:60px"',
 									), // attributes to add to input element
+				'class'				=> '', // class(es) to add to input
+				'field_attributes'	=> array(), // attr => value array for field container
+				'field_class'		=> '', // class(es) to add to field container
+				'custom_sanitize'	=> '', // function to do additional sanitization (or array( &$this, 'method' ))
+				'custom_field'		=> '', // function for custom display of field input
+				'taxonomies'		=> array(), // hide field if taxonomies are not supported
+			),
+
+			// Attachment
+			'attachment' => array(
+				'name'				=> _x( 'Attachment', 'section widget', 'saved' ),
+				'after_name'		=> '', // (Optional), (Required), etc.
+				'desc'				=> '',
+				'type'				=> 'radio', // text, textarea, checkbox, radio, select, number, url, image, color
+				'checkbox_label'	=> '', // show text after checkbox
+				'radio_inline'		=> true, // show radio inputs inline or on top of each other
+				'number_min'		=> '', // lowest possible value for number type
+				'number_max'		=> '', // highest possible value for number type
+				'options'			=> array( // array of keys/values for radio or select
+					'scroll'	=> _x( 'Scroll', 'section widget attachment', 'church-theme-framework' ),
+					'fixed'		=> _x( 'Fixed', 'section widget attachment', 'church-theme-framework' ),
+				),
+				'upload_button'		=> '', // for url field; text for button that opens media frame
+				'upload_title'		=> '', // for url field; title appearing at top of media frame
+				'upload_type'		=> '', // for url field; optional type of media to filter by (image, audio, video, application/pdf)
+				'default'			=> 'scroll', // value to pre-populate option with (before first save or on reset)
+				'no_empty'			=> true, // if user empties value, force default to be saved instead
+				'allow_html'		=> false, // allow HTML to be used in the value (text, textarea)
+				'attributes'		=> array(), // attributes to add to input element
 				'class'				=> '', // class(es) to add to input
 				'field_attributes'	=> array(), // attr => value array for field container
 				'field_class'		=> '', // class(es) to add to field container
