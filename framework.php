c95ff921b5c312b206c71331de929597f9ad6699
Steven Gliebe <work@stevengliebe.com>
2017-09-07 14:19:41 -0500

    Bump version

--- framework.php
+++ framework.php
@@ -29,7 +29,7 @@ $theme_data = is_child_theme() ? wp_get_theme( $theme_data->template ) : $theme_
 /**
  * Framework constants
  */
-if ( ! defined( 'CTFW_VERSION' ) )				define( 'CTFW_VERSION', 			'2.0' );
+if ( ! defined( 'CTFW_VERSION' ) )				define( 'CTFW_VERSION', 			'2.1' );
 
 /**
  * Theme constants
