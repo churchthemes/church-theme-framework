990e112e437a9a2dd3868bc88e3e81b384452d3e
Steven Gliebe <work@stevengliebe.com>
2017-11-07 10:51:28 -0600

    Fix syntax error

--- includes/events.php
+++ includes/events.php
@@ -742,7 +742,7 @@ function ctfw_event_calendar_data( $args ) {
 					'frequency'      => $event_data['recurrence'], 				// weekly, monthly, yearly
 					'interval'       => $interval, 								// every X weeks, months or years.
 					'weekly_type'    => $event_data['recurrence_weekly_type'], 	// 'same' (same day of week) or 'day' (on specific days(s)); if recurrence is weekly ('same' is default).
-					'weekly_day'     => $event_data['recurrence_weekly_day']	// single value, array or JSON-encoded array of day of week in 2-letter format (SU, MO, TU, etc.). If empty, uses same day of week.
+					'weekly_day'     => $event_data['recurrence_weekly_day'],	// single value, array or JSON-encoded array of day of week in 2-letter format (SU, MO, TU, etc.). If empty, uses same day of week.
 					'monthly_type'   => $event_data['recurrence_monthly_type'],	// 'day' (same day of month) or 'week' (on specific week(s)); if recurrence is monthly ('day' is default).
 					'excluded_dates' => $event_data['excluded_dates'],			// dates to exclude in YYYY-mm-dd format, separated by comma, as array or JSON-encoded array.
 					'monthly_week'   => $event_data['recurrence_monthly_week'], // single value, array or JSON-encoded array of numeric week(s) of month (or 'last') (e.g. 1, 2, 3, 4, 5 or last).
