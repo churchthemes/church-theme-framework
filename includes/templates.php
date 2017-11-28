a8329b9ce690f73c39aa3d8e8607d2f58a6fe70b
Steven Gliebe <work@stevengliebe.com>
2017-11-28 13:15:23 -0600

    Make ctfw_get_content_template() check if post type exists to avoid loading a post type's template when Church Content plugin inactive

--- includes/templates.php
+++ includes/templates.php
@@ -70,11 +70,18 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  */
 function ctfw_get_content_template() {
 
-	// Templates will be attempted to be loaded in the order they are added to this array
+	// Templates will be attempted to be loaded in the order they are added to this array.
 	$templates = array();
 
-	// Get post type
+	// Get post type.
 	$post_type = get_post_type();
+
+	// Post type must be supported.
+	if ( ! post_type_exists( $post_type ) ) {
+		return;
+	}
+
+	// Get friendly post type.
 	$post_type_friendly = ctfw_make_friendly( $post_type ); // "ctc_post_type" is made into "post-type" for friendlier template naming
 
 	// Singular post?
