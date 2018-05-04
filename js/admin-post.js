0278c5c147b994583e6b71333752123f5eee5502
Steven Gliebe <work@stevengliebe.com>
2018-05-04 11:52:22 -0500

    Reorganize js/admin-post.js

--- js/admin-post.js
+++ js/admin-post.js
@@ -5,22 +5,28 @@
 jQuery( document ).ready( function( $ ) {
 
 	/*******************************************
-	 * FEATURED IMAGES
+	 * GUTENBERG EDITOR
 	 *******************************************/
 
-	// Add size notes to Gutenberg's Featured Image box for custom post types.
-	// This is handled in classic editor by filtering.
-	setTimeout( function() {
+	 // Gutenberg in use.
+	 if ( $( '.gutenberg' ).length ) {
 
-		if ( $( '.gutenberg .editor-post-featured-image' ).length && ctfw_post.featured_image_note.length ) { // Gutenberg editor and have post type note.
+		// After Gutenberg inits.
+		setTimeout( function() {
 
-				$( '.gutenberg .editor-post-featured-image' )
-					.append( '<p class="description" id="ctfw-featured-image-note">' + ctfw_post.featured_image_note + '</p>' );
-				$( '#ctfw-featured-image-note' )
-					.hide()
-					.fadeIn( 'fast' );
-		}
+			// Add size notes to Gutenberg's Featured Image box for custom post types.
+			// This is handled in classic editor by filtering.
+			if ( $( '.gutenberg .editor-post-featured-image' ).length && ctfw_post.featured_image_note.length ) { // Gutenberg editor and have post type note.
 
-	}, 100 ); // after Gutenberg.
+					$( '.gutenberg .editor-post-featured-image' )
+						.append( '<p class="description" id="ctfw-featured-image-note">' + ctfw_post.featured_image_note + '</p>' );
+					$( '#ctfw-featured-image-note' )
+						.hide()
+						.fadeIn( 'fast' );
+			}
+
+		}, 100 );
+
+	}
 
 } );
