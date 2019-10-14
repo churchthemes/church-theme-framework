8fab7b9f7c33bc7f8b18937a10b07cd8153278f5
Steven Gliebe <3372396+stevengliebe@users.noreply.github.com>
2019-10-14 07:22:17 -0500

    Change ctfw-clean-video-shortcode-url to ctfw-clean-media-shortcode-url to affect audio shortcode too

--- includes/embeds.php
+++ includes/embeds.php
@@ -215,23 +215,25 @@ function ctfw_valid_embeds( $html ) {
 add_filter( 'embed_oembed_html', 'ctfw_valid_embeds' );
 
 /**
- * Clean video shortcode URLs
+ * Clean media shortcode URLs
  *
  * Safari sometimes struggles with video shortcode's src URL when ?_=1 is appended.
- * Remove it with add_theme_support( 'ctfw-clean-video-shortcode-url' );
+ * Remove it with add_theme_support( 'ctfw-clean-media-shortcode-url' );
+ *
+ * This may also help with firewall cache issues.
  *
  * @since 2.6.4
- * @param string $output Video shortcode HTML output.
+ * @param string $output Audio or video shortcode HTML output.
  * @param array $atts Array of video shortcode attributes.
- * @param string $video Video file.
+ * @param string $media Audio or video file.
  * @param int $post_id Post ID.
  * @param string $library Media library used for the video shortcode.
  * @return string Filtered video output.
  */
-function ctfw_clean_video_shortcode_url( $output, $atts, $video, $post_id, $library ) {
+function ctfw_clean_media_shortcode_url( $output, $atts, $media, $post_id, $library ) {
 
 	// Theme supports this.
-	if ( current_theme_supports( 'ctfw-clean-video-shortcode-url' ) ) {
+	if ( current_theme_supports( 'ctfw-clean-media-shortcode-url' ) ) {
 
 		// Remove ?_=1 from video URL.
 		$output = str_replace( '?_=1', '', $output );
@@ -242,5 +244,6 @@ function ctfw_clean_video_shortcode_url( $output, $atts, $video, $post_id, $libr
 
 }
 
-add_action( 'wp_video_shortcode', 'ctfw_clean_video_shortcode_url', 10, 5 );
+add_action( 'wp_audio_shortcode', 'ctfw_clean_media_shortcode_url', 10, 5 );
+add_action( 'wp_video_shortcode', 'ctfw_clean_media_shortcode_url', 10, 5 );
 
